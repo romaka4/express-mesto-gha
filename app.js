621c20931856f323becc88ddb4e8c2ef670d81d1
romaka4 <rkafo@yandex.ru>
2024-01-17 19:12:25 +0300

    project 13

--- app.js
+++ app.js
@@ -4,17 +4,8 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// const { PORT = 3000 } = process.env;
-const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/mestodb'} = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb'} = process.env;
 mongoose.connect(DB_URL);
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-//  app.use('/', (req, res, next) =>{
-//   console.log(req.headers);
-//   res.status(200).send({message: "Ответ2"});
-//   next();
-//  });
 app.use((req, res, next) => {
   req.user = {
     _id: '65a42cdb0f1e71717498e6c3'
@@ -25,16 +16,6 @@ app.use((req, res, next) => {
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 app.listen(PORT, () => {
-  // Если всё работает, консоль покажет, какой порт приложение слушает
   console.log(`App listening on port to ${PORT}`)
 }) 
 
-// mongoose.connect('mongodb://localhost:27017/mydb', {
-//   useNewUrlParser: true,
-//   useCreateIndex: true,
-//     useFindAndModify: false
-// });
-
-// // подключаем мидлвары, роуты и всё остальное...
-
-// app.listen(3000); 
