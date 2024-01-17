5bc4ac8ea2444285f0fceae2538042d86f3e14a0
romaka4 <rkafo@yandex.ru>
2024-01-17 19:17:00 +0300

    project 13

--- routes/cards.js
+++ routes/cards.js
@@ -5,5 +5,4 @@ router.post('/', createCard);
 router.delete('/:cardId', deleteCardById);
 router.put('/:cardId/likes', likeCard);
 router.delete('/:cardId/likes', dislikeCard);
-
 module.exports = router;
