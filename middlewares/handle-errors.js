d5986d7a750531fe4e20f44e052fffcdef0f2743
romaka4 <rkafo@yandex.ru>
2024-01-25 10:50:58 +0300

    project 14/2

--- middlewares/handle-errors.js
+++ middlewares/handle-errors.js
@@ -30,30 +30,4 @@ module.exports = ((err, req, res, next) => {
     });
   }
   next();
-  // if (err.name === 'NotFoundError') {
-  //   console.log('i am');
-  //   res.status(err.statusCode).send({ messaege: err.message });
-  //   return;
-  // } else {
-  //   res.status(500).send({ message: 'Произошла ошибка на стороне сервера' });
-  // }
-  // if (!err.statusCode) {
-  //   const { statusCode = 500, message } = err;
-  //   res.status(statusCode).send({
-  //     message: statusCode === 500 ? 'На сервере произошла ошибка'
-  //       : message,
-  //   });
-  // }
-  // res.status(err.statusCode).send({ message: err.message });
-  // next();
-  // if (err.statusCode) {
-  //   res.status(err.statusCode).send({ message: err.message });
-  // } else {
-  //   const { statusCode = 500, message } = err;
-  //   res.status(statusCode).send({
-  //     message: statusCode === 500 ? 'На сервере произошла ошибка'
-  //       : message,
-  //   });
-  // }
-  // next();
 });
